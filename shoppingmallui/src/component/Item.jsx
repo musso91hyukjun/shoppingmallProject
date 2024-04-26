cbdec3b6ce23bb6620877092fa3db8b51b59b951
Insik Jeong <150884733+InSik-j@users.noreply.github.com>
2024-04-26 14:17:31 +0900

    상품 목록 리팩터링

--- shoppingmallui/src/component/Item.jsx
+++ shoppingmallui/src/component/Item.jsx
@@ -1,63 +1,61 @@
 import item from '../image/item.jpg'
 import styles from '../css/Item.module.css'
 import cn from 'classnames';
+import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
 
 export default function Item() {
+    const [currentCategory, setCurrentCategory] = useState('OUTWEAR');
+    const [items, setItems] = useState([]);
+  
+    useEffect(() => {
+      axios.get(`http://localhost:8080/api/items/MAN_${currentCategory}`)
+        .then(response => {
+          setItems(response.data);
+        })
+        .catch(error => console.error('Error fetching products:', error));
+    }, [currentCategory]); // 의존성 배열에 currentCategory 추가
+  
+    // 카테고리 클릭 핸들러
+    const handleCategoryClick = (category) => {
+      setCurrentCategory(category);
+    };
+  
 
     return (
         <div className={cn(styles.content, "container")}>
             <div className="category">
                 <p className={`${styles.categoryTitle} fw-bold`}>Man</p>
                 <ul className={`${styles.categoryList} d-flex justify-content-center gap-4`}>
-                    <li>OUTWEAR</li>
-                    <li>TOP</li>
-                    <li>BOTTOM</li>
-                    <li>DRESS</li>
-                    <li>ACC</li>
+                    <li onClick={() => handleCategoryClick('OUTWEAR')}>OUTWEAR</li>
+                    <li onClick={() => handleCategoryClick('TOP')}>TOP</li>
+                    <li onClick={() => handleCategoryClick('BOTTOM')}>BOTTOM</li>
+                    <li onClick={() => handleCategoryClick('ACC')}>ACC</li>
                 </ul>
             </div>
             <ul className={`${styles.itemList} d-grid gap-4`}>
-                <li className={`${styles.item}`}>
-                    <img className={`${styles.itemImg}`} src={item} />
-                    <div className={`${styles.itemInfo}`}>
-                        <p className={`${styles.itemName} fw-bold`} >Chocho wide denim</p>
-                        <p>2컬러/</p>
-                        <p className={`${styles.itemPrice}`}>59,000</p>
-                    </div>
-                </li>
-                <li className={`${styles.item}`}>
-                    <img className={`${styles.itemImg}`} src={item} />
-                    <div className={`${styles.itemInfo}`}>
-                        <p className={`${styles.itemName} fw-bold`} >Chocho wide denim</p>
-                        <p>2컬러/</p>
-                        <p className={`${styles.itemPrice}`}>59,000</p>
-                    </div>
-                </li>
-                <li className={`${styles.item}`}>
-                    <img className={`${styles.itemImg}`} src={item} />
-                    <div className={`${styles.itemInfo}`}>
-                        <p className={`${styles.itemName} fw-bold`} >Chocho wide denim</p>
-                        <p>2컬러/</p>
-                        <p className={`${styles.itemPrice}`}>59,000</p>
-                    </div>
-                </li>
-                <li className={`${styles.item}`}>
-                    <img className={`${styles.itemImg}`} src={item} />
-                    <div className={`${styles.itemInfo}`}>
-                        <p className={`${styles.itemName} fw-bold`} >Chocho wide denim</p>
-                        <p>2컬러/</p>
-                        <p className={`${styles.itemPrice}`}>59,000aa</p>
-                    </div>
-                </li>
-                <li className={`${styles.item}`}>
-                    <img className={`${styles.itemImg}`} src={item} />
-                    <div className={`${styles.itemInfo}`}>
-                        <p className={`${styles.itemName} fw-bold`} >Chocho wide denim</p>
-                        <p>2컬러/</p>
-                        <p className={`${styles.itemPrice}`}>59,000</p>
-                    </div>
-                </li>
+                {items.map(item => (
+                    <li className={`${styles.item}`} key={item.id}>
+                        <img className={`${styles.itemImg}`} src={item} />
+                        <div className={`${styles.itemInfo}`}>
+                            <p className={`${styles.itemName} fw-bold`} >{item.name}</p>
+                            <p>{item.color}</p>
+                            <p className={`${styles.itemPrice}`}>{item.price}</p>
+                        </div>
+                    </li>
+                ))}
+               
             </ul>
         </div>
     )
 }
+{/* <li className={`${styles.item}`}>
+<img className={`${styles.itemImg}`} src={item} />
+<div className={`${styles.itemInfo}`}>
+    <p className={`${styles.itemName} fw-bold`} >Chocho wide denim</p>
+    <p>2컬러/</p>
+    <p className={`${styles.itemPrice}`}>59,000</p>
+</div>
+</li> */}
